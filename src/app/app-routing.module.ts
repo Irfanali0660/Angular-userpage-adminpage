c95c1fdb2b4a8d322e3e42750fdce2f397d70eed
irfanali <irfu3844@gmail.com>
2023-03-02 21:10:06 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { LoginComponent } from './page/login/login.component';
+import { SignupComponent } from './page/signup/signup.component';
+import { AuthGuard } from './auth-service/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent,canActivate: [AuthGuard]},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
